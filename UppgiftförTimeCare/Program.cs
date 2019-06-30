30fde48b3f8c3cf89a861104dd64a35a0cbde695
Andreas Eklund <andy_yoman@hotmail.com>
2019-06-30 20:39:16 +0200

    fixa

---
+++ UppgiftförTimeCare/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace UppgiftförTimeCare
+{
+
+    //class Animal {
+
+    //    void write(string a) {
+
+    //        Console.WriteLine("Old McDonald had a farm \n" +
+    //                                  "E - I - E - I - O\n " +
+    //                                  "And on his farm he had a {0}", a "\n" +
+    //                                  " E - I - E - I - O\n" +
+    //                                  "With a moo moo here\n" +
+    //                                  "and a moo moo there\n" +
+    //                                  "here a moo, there a moo, everywhere a moo moo.\n"
+    //                                                            );
+    //    }
+    //}
+
+    //static void Main(string[] args)
+    //{
+    //    Animal a = new Animal();
+    //    a.write("cow");
+    //}
+    //{
+    //    Console.WriteLine("Old McDonald had a farm \n" +
+    //                              "E - I - E - I - O\n " +
+    //                              "And on his farm he had a cow\n" +
+    //                              " E - I - E - I - O\n" +
+    //                              "With a moo moo here\n" +
+    //                              "and a moo moo there\n" +
+    //                              "here a moo, there a moo, everywhere a moo moo.\n"
+    //                                                        );
+    //    Console.WriteLine("Old McDonald had a farm \n" +
+    //                              "E - I - E - I - O\n " +
+    //                              "And on his farm he had a dog\n" +
+    //                              " E - I - E - I - O\n" +
+    //                              "With a woof woof here\n" +
+    //                              "and a woof woof there\n" +
+    //                              "here a woof, there a woof, everywhere a woof woof.\n"
+    //                                                        );
+    //    Console.WriteLine("Old McDonald had a farm \n" +
+    //                              "E - I - E - I - O\n " +
+    //                              "And on his farm he had a cat\n" +
+    //                              " E - I - E - I - O\n" +
+    //                              "With a mjau mjau here\n" +
+    //                              "and a mjau mjau there\n" +
+    //                              "here a mjau, there a mjau, everywhere a mjau mjau.\n"
+    //                                                        );
+    //    Console.WriteLine("Old McDonald had a farm \n" +
+    //                              "E - I - E - I - O\n " +
+    //                              "And on his farm he had a pig\n" +
+    //                              " E - I - E - I - O\n" +
+    //                              "With a nöff nöff here\n" +
+    //                              "and a nöff nöff there\n" +
+    //                              "here a nöff, there a nöff, everywhere a nöff nöff.\n"
+    //                                                        );
+    //    Console.WriteLine("Old McDonald had a farm \n" +
+    //                              "E - I - E - I - O\n " +
+    //                              "And on his farm he had a duck\n" +
+    //                              " E - I - E - I - O\n" +
+    //                              "With a quack quack here\n" +
+    //                              "and a quack quack there\n" +
+    //                              "here a quack, there a quack, everywhere a quack quack.\n"
+    //                                                        );
+    //Console.ReadKey();
+
+
+
+
+    //int caseswitch = 1;
+
+
+    //switch (caseswitch)
+    //{
+    //    case 1:
+    //Console.WriteLine("Old McDonald had a farm"+ 
+    //                  "E - I - E - I - O " +
+    //                  "And on his farm he had a dog" +
+    //                  " E - I - E - I - O" +
+    //                  "With a woof woof here"+
+    //                  "and a woof woof there"+
+    //                  "here a woof, there a woof, everywhere a woof woof."
+    //                                            );
+    //        break;
+    //    case 2:
+    //        Console.WriteLine("Old McDonald had a farm" +
+    //                          "E - I - E - I - O " +
+    //                          "And on his farm he had a cow" +
+    //                          " E - I - E - I - O" +
+    //                          "With a moo moo here" +
+    //                          "and a moo moo there" +
+    //                          "here a moo, there a moo, everywhere a moo moo."
+    //                                                    );
+    //        break;
+
+
+
+    //}
+
+
+
+
+
+    
+}
---
+++ UppgiftförTimeCare/polmorphismversion.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UppgiftförTimeCare
+{
+    // kom på senare att det skulle va polymorphism men tyckte det här var smidigare.
+    // jag försökte lite med polymorphism, jag har kommenterat bort det.
+    class Animal { 
+                          
+
+        static void Main(string[] args)
+        {
+            var animal = new[] { "cow", "dog", "cat", "duck", "pig" };
+            var sound = new[] { "moo", "woof", "mjau", "quack", "nöff" };
+            var anso = animal.Zip(sound, (a, s) => new { Word1 = a, Word2 = s });
+
+            foreach (var a in anso)
+            {
+                Console.WriteLine("Old McDonald had a farm \n" +
+                                      "E - I - E - I - O\n " +
+                                      "And on his farm he had a"+" "+ a.Word1+ "\n" +
+                                      " E - I - E - I - O\n" +
+                                      "With a"+" "+ a.Word2 +" "+ a.Word2+ " "+"here\n" +
+                                      "and a" + " " + a.Word2 + " " + a.Word2 + " " + "  there\n" +
+                                      "here a"+" " + a.Word2+", there a"+" "+a.Word2+" , everywhere a"+" "+a.Word2 +" " +a.Word2+"\n"
+                                                                );
+            }
+            var animal2 = new[] { "cow", "dog", "cat", "duck", "pig","rooster" };//etc
+            var sound2 = new[] { "moo", "woof", "mjau", "quack", "nöff", "kukkeliku" };//etc
+            var anso2 = animal.Zip(sound, (a, s) => new { Word1 = a, Word2 = s });
+
+            foreach (var a in anso2)
+            {
+                Console.WriteLine("Old McDonald had a farm \n" +
+                                      "E - I - E - I - O\n " +
+                                      "And on his farm he had a" + " " + a.Word1 + "\n" +
+                                      " E - I - E - I - O\n" +
+                                      "With a" + " " + a.Word2 + " " + a.Word2 + " " + "here\n" +
+                                      "and a" + " " + a.Word2 + " " + a.Word2 + " " + "  there\n" +
+                                      "here a" + " " + a.Word2 + ", there a" + " " + a.Word2 + " , everywhere a" + " " + a.Word2 + " " + a.Word2 + "\n"
+                                                                );
+            }
+
+
+            string input;
+            Console.Write("Please write an animal of your own preference\n");
+            Console.ReadKey();
+            input = Console.ReadLine();
+            string input2;
+            Console.Write("Please write which sound you want it to have of your own preference\n");
+            input2 = Console.ReadLine();
+            Console.WriteLine("Old McDonald had a farm \n" +
+                                  "E - I - E - I - O\n " +
+                                  "And on his farm he had a"+" "+input+"\n" +
+                                  " E - I - E - I - O\n" +
+                                  "With a" +" "+input2 +" " +input2 +"here\n" +
+                                  "and a"+" " + input2 + " "+ input2+ "there\n" +
+                                  "here a" + " "+ input2+ ", there a"+" " + input2+", everywhere a" +" " + input2 + " "+ input2+".\n"
+                                                            );
+            Console.ReadKey();
+
+        }
+    }
+}
